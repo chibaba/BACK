a1534874cc40d6242df16d43d5f444f69eb0d2e1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-02 21:12:39 +0100

    finduserbyid and read functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -28,9 +28,22 @@ const list = (req, res) => {
     }).select('name email updated created')
 }
 
-const userById = (req, res, next, id) => {}
+const userById = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+        if(err || !user)
+        return res.status(400).json({
+            error: "User not found"
+        })
+        req.profile = user
+        next()
+    })
+}
 
-const read = (req, res) => {}
+const read = (req, res) => {
+        req.profile.hashed_password = undefined
+        req.profile.salt = undefined
+        return res.json(req.profile)
+}
 
 const update = (req, res, next) => {}
 
