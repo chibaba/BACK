8bebe3d021165689ee51703b28ea2903a68aa5e0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-02 01:00:12 +0100

    created the user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,18 @@
+import User from  '../models/user.model'
+
+import _ from 'loadash'
+import errorHandler from './error.controller.js'
+
+const create = (req, res, next) => {}
+
+const list = (req, res) => {}
+
+const userById = (req, res, next, id) => {}
+
+const read = (req, res) => {}
+
+const update = (req, res, next) => {}
+
+const remove = (req, res, next) => {}
+
+export default {create, userById, read, list, remove, update}
--- server/express.js
+++ server/express.js
@@ -5,15 +5,18 @@ import compress from  'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 import Template from './../template'
+import userRoutes from './routes/user.routes'
 
 
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser);
 app.use(helmet());
 app.use(cors());
+app.use('/', userRoutes)
 
 
 app.get('/', (req, res) => {
