364479477fe4773f16539ed3117e26c0d1271571
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-07 10:50:06 +0100

    style declaration

---
+++ client/core/Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import Card, {CardContent, CardMedia} from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import seashelllmg from './../assets/images/seashell/jpg'
+
+
+const styles= theme =>({
+    card: {
+        maxWidth: 600,
+        margin: 'auto',
+        marginTop: theme.spacing.unit* 5
+    },
+    title: {
+        padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px
+            ${theme.spacing.unit*2}px`,
+            color: theme.palette.text.secondary
+    },
+    media: {
+        minHeight: 330
+    }
+})
