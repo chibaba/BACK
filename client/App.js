b120bb1a060b85b157ef2eb771be30ef8780cab4
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-07 09:30:31 +0100

    wip

---
+++ client/App.js
@@ -0,0 +1,22 @@
+import {MuiThemeProvider, CrateMuiTheme} from 'material-ui/styles';
+import {indigo, pink} from 'material-ui/colors'
+import { createMuiTheme } from 'material-ui';
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#757de8',
+            main: '#3f51b5',
+            dark: '#002984',
+            contrastText: '#fff'
+        },
+        secondary: {
+            light: '#ff79b0',
+            main: '#ff4081',
+            dark: '#c60055',
+            contrastText: '#000'
+        },
+        openTitle: indigo['400'],   
+        protectedTitle:pink['400'],
+        type: 'light'
+    }
+})
