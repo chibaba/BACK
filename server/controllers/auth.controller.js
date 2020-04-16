c1676ad6d065314d8e5e35c4c70aed9509d8d995
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-16 17:56:26 +0100

    setting up finals

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,55 +1,65 @@
 import User from '../models/user.model'
-import jwt  from 'jsonwebtoken'
-import expressJwt from './../../config/config'
-import { request } from 'express'
+import jwt from 'jsonwebtoken'
+import expressJwt from 'express-jwt'
+import config from './../../config/config'
 
-const signIn = (req, res) => {
-    User.findOne({
-        "email":req.body.email
-    }, (err, user) => {
-        if(err || !user)
-        return res.status('401').json({
-            error: "User not found"
-        })
-        if(!user.authenticate(req.body.password)) {
-            return res.status('401').send({
-                error: "Email and Password does not match"
-            })
-        }
-        const token = jwt.sign({
-            _id:user.id
-        },config.jwtSecret)
-        res.cookie("t", token, {
-            expire: new Date() + 9999
-        } )
-        return res.json({
-            token,
-            user: {_id: user.id, name: user.name, email: user.email}
-        })
-    })
-}
+const signin = (req, res) => {
+  User.findOne({
+    "email": req.body.email
+  }, (err, user) => {
 
-const signOut = (req, res) => {
-    res.clearCookie("t")
-    return res.status('200').json({
-        message: "signed out"
-    })
-}
+    if (err || !user)
+      return res.status('401').json({
+        error: "User not found"
+      })
 
-const requireSignIn = expressJwt({
-    secret: config.jwtSecret,
-    userProperty: 'auth'
-})
-const hasAuthorization = (req, res,next) => {
-    const authorized = req.profile && req.auth && request.profile_id ==
-    req.auth.id
-    if(!(authorized)) {
-        return res.status('403').json({
-            error: 'User is not authorized'
-        })
+    if (!user.authenticate(req.body.password)) {
+      return res.status('401').send({
+        error: "Email and password don't match."
+      })
     }
-    next()
+
+    const token = jwt.sign({
+      _id: user._id
+    }, config.jwtSecret)
+
+    res.cookie("t", token, {
+      expire: new Date() + 9999
+    })
+
+    return res.json({
+      token,
+      user: {_id: user._id, name: user.name, email: user.email}
+    })
+
+  })
 }
 
+const signout = (req, res) => {
+  res.clearCookie("t")
+  return res.status('200').json({
+    message: "signed out"
+  })
+}
 
-export default { signIn, signOut, requireSignIn, hasAuthorization}
+const requireSignin = expressJwt({
+  secret: config.jwtSecret,
+  userProperty: 'auth'
+})
+
+const hasAuthorization = (req, res, next) => {
+  const authorized = req.profile && req.auth && req.profile._id == req.auth._id
+  if (!(authorized)) {
+    return res.status('403').json({
+      error: "User is not authorized"
+    })
+  }
+  next()
+}
+
+export default {
+  signin,
+  signout,
+  requireSignin,
+  hasAuthorization
+}
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import authCtrl from '../controllers/auth.controller';
+import authCtrl from '../controllers/auth.controller'
 
 const router = express.Router()
-router.route('/auth/signIn')
- .post(authCtrl.signIn)
-router.route('auth/signOut')
-.get(authCtrl.signOut)
 
+router.route('/auth/signin')
+  .post(authCtrl.signin)
+router.route('/auth/signout')
+  .get(authCtrl.signout)
 
 export default router
